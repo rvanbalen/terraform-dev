ff650ed7e71a51d13cb1da7c1d51ab709c49d99b
René van Balen <rene.van.balen@ictivity.nl>
2025-06-26 15:30:13 +0200

    Add initial Terraform configuration and variables for Azure provider

---
+++ terraform/provider.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+  required_version = ">= 1.9, < 2.0"
+}
+
+provider "azurerm" {
+  # Credentials are passed either through Azure CLI or environment variables
+  subscription_id = var.subscription_id_management
+
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+    key_vault {
+      purge_soft_delete_on_destroy    = true
+      recover_soft_deleted_key_vaults = true
+    }
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "primary_location" {
+  type    = string
+  default = "westeurope"
+}
+
+variable "subscription_id_management" {
+  type        = string
+  description = "Subscription ID to be used for resources"
+}
